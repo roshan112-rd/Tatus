346175c64a8c02bc5e02d2f1b19a0ab8a2f21f34
roshan112-rd <58673204+roshan112-rd@users.noreply.github.com>
2022-03-28 20:54:40 +0545

    sql

--- tatus/settings.py
+++ tatus/settings.py
@@ -85,6 +85,7 @@ WSGI_APPLICATION = 'tatus.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
